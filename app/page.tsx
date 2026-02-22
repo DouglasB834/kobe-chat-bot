09fb7f8f701317e65d09c1e5ded632d495704252
DouglasB834 <douglas_borges09@hotmail.com>
2026-02-21 21:04:59 -0300

    Update configuration and dependencies for chat functionality
    - Added image domain configuration for Unsplash in next.config.ts.
    - Updated package.json and package-lock.json to include new dependencies: @hookform/resolvers, motion, react-hook-form, and zod.
    - Enhanced page.tsx to integrate ChatContainer and display chat messages.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,28 @@
-import { ComponentExample } from "@/components/component-example";
+import { ChatContainer } from "@/components/chat/ChatContainer";
+import { MessageBubble } from "@/components/chat/message-bubble";
+import { Message } from "@/types/message";
+import { div } from "motion/react-client";
+// import { ComponentExample } from "@/components/component-example";
+
+const fakeMessages: Message[] = [
+  {
+    id: "1",
+    role: "user",
+    text: "Olá, estou procurando por um anel solitário em ouro amarelo ou branco com diamantes para um pedido de casamento. Quero opções que custem até cinco mil reais.",
+    createdAt: new Date(),
+  },
+  {
+    id: "2",
+    role: "assistant",
+    text: "Claro, aqui estão algumas sugestões de anéis solitário com as características e dentro do valor que você solicitou ",
+    createdAt: new Date(),
+  },
+];
 
 export default function Page() {
-return <ComponentExample />;
+  return (
+    <div className="h-screen w-full flex items-center ">
+        <ChatContainer/>
+    </div>
+  );
 }
